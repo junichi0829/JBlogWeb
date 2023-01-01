c5a4177e59eebbb6fe93163360640cfa6b917eb8
junichi0829 <junjundesuu@yahoo.co.jp>
2023-01-01 22:37:38 +0900

    Userを修正。

--- JBlogWeb/src/main/java/com/junichi/jblog/domain/User.java
+++ JBlogWeb/src/main/java/com/junichi/jblog/domain/User.java
@@ -1,5 +1,16 @@
 package com.junichi.jblog.domain;
 
+import java.security.Timestamp;
+
+import javax.persistence.Column;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+import org.hibernate.annotations.CreationTimestamp;
+
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -11,9 +22,26 @@ import lombok.NoArgsConstructor;
 @Builder
 public class User {
 	
+	//主キーに対応する識別子の変数
+	@Id
+	
+	// 1から始まって自動的に1ずつ増加するようにストラテジーを設定
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private int id; 			// 会員番号
+	
+	@Column(nullable = false, length = 50, unique = true)
 	private String username;	// アカウント
+	
+	@Column(length = 100)
 	private String password;	// パスワード
+	
+	@Column(nullable = false, length = 100)
 	private String email;		// メールアドレス
+	
+	@Enumerated(EnumType.STRING)
+	private RoleType role;
+	
+	@CreationTimestamp // 現在の時間がデフォルトに登録されるように設定
+	private Timestamp createDate;
 	
 }
