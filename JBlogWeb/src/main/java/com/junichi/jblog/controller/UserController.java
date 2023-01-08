934da0c6bb750fc1e49bd692c1d8731f06c53e24
junichi0829 <junjundesuu@yahoo.co.jp>
2023-01-08 22:11:39 +0900

    UserController.javaを作成。

---
+++ JBlogWeb/src/main/java/com/junichi/jblog/controller/UserController.java
@@ -0,0 +1,28 @@
+package com.junichi.jblog.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import com.junichi.jblog.domain.RoleType;
+import com.junichi.jblog.domain.User;
+import com.junichi.jblog.persistence.UserRepository;
+
+@Controller
+public class UserController {
+	
+	@Autowired
+	private UserRepository userRepository;
+	
+	@PostMapping("/user")
+	public @ResponseBody String insertUser(@RequestBody User user) {
+		
+		user.setRole(RoleType.USER);
+		userRepository.save(user);
+		return user.getUsername() + "会員登録ができました。";
+		
+	}
+	
+}
