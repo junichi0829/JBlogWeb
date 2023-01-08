6204976210610509254125fe331bd2601b3ec2c0
junichi0829 <junjundesuu@yahoo.co.jp>
2023-01-08 22:31:24 +0900

    UserController.javaに会員情報を見るメソッドを作成。

--- JBlogWeb/src/main/java/com/junichi/jblog/controller/UserController.java
+++ JBlogWeb/src/main/java/com/junichi/jblog/controller/UserController.java
@@ -2,6 +2,8 @@ package com.junichi.jblog.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -22,6 +24,15 @@ public class UserController {
 		user.setRole(RoleType.USER);
 		userRepository.save(user);
 		return user.getUsername() + " 会員登録ができました。";
+		
+	}
+	
+	@GetMapping("/user/get/{id}")
+	public @ResponseBody User getUser(@PathVariable int id) {
+		
+		// 特定のid（会員番号）に該当するUserオブジェクトを返還する。
+		User findUser = userRepository.findById(id).get();
+		return findUser;
 		
 	}
 	
